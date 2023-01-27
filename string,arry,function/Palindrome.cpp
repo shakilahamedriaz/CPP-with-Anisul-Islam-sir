2e6085ec1d37526437c899ff1781814e351b1a7d
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-01-28 02:20:01 +0600

    af

---
+++ string,arry,function/Palindrome.cpp
@@ -0,0 +1,25 @@
+#include<bits/bits/stdc++.h
+using namespace std;
+int main(){
+
+string s;
+cin>>s;
+
+string p=s;
+
+
+reverse(p.begain(), p.end());
+
+if(s==p){
+
+    return "YEs";
+}
+else{
+
+    return "No";
+}
+}
+
+
+return 0;
+}
